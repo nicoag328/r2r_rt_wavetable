0e44692fa5e6fa6cc98b31aa53488f5241fe2e6e
nicoag328 <78130471+nicoag328@users.noreply.github.com>
2022-09-24 17:38:29 -0300

    Changed input method. Moved envelope

---
+++ env.c
@@ -0,0 +1,114 @@
+// env.c
+
+enum {
+	ENV_ATTACK = 0,
+	ENV_SUSTAIN,
+	ENV_RELEASE
+};
+
+
+volatile struct env_t {
+	int8_t sample;
+	uint8_t state;
+	uint32_t level;
+	uint32_t attack_dx;
+	uint32_t release_dx;
+
+} env_a;
+
+void env_init(volatile struct env_t* env)
+{
+	env->sample = 0;
+	env->state = ENV_RELEASE;
+	env->level = 0;
+	env->attack_dx = 100;
+	env->release_dx = 3;
+}
+
+//TODO rewrite state machine
+void env_change_state(volatile struct env_t* env)
+{
+	switch(env->state)
+	{
+		case ENV_ATTACK:
+			env->state = ENV_SUSTAIN;
+			break;
+
+		case ENV_SUSTAIN:
+			env->state = ENV_RELEASE;
+			env->level = 0xFF<<11;
+			break;
+
+		case ENV_RELEASE:
+			env->state = ENV_ATTACK;
+			env->level = 0;
+			break;
+	}
+}
+
+void env_gate(volatile struct env_t* env, bool gate)
+{
+	if(gate == true)
+	{
+		if(env->state == ENV_RELEASE)
+		{
+			env->state = ENV_ATTACK;
+		}
+	}
+	else
+	{
+		env->state = ENV_RELEASE;
+	}	
+}
+
+void env_set_attack(volatile struct env_t* env, uint32_t attack_time)
+{
+	env->attack_dx = 0xFF<<11 / attack_time;
+}
+
+void env_set_release(volatile struct env_t* env, uint32_t release_time)
+{
+	env->release_dx = 0xFF<<11 / release_time;
+}
+
+
+// TODO Optimization
+int8_t env_run(volatile struct env_t* env, int8_t input)
+{
+	if(env->state == ENV_ATTACK)
+	{
+		if (env->level >= 0xFF<<11)
+			env_change_state(env);
+		else
+			env->level += env->attack_dx;
+	}
+
+	if(env->state == ENV_RELEASE)
+	{
+		if (env->level < env->release_dx)
+			env->level = 0;
+		else
+			env->level -= env->release_dx;
+	}
+
+	bool is_neg = false;
+	volatile uint32_t temp;
+
+	if(input < 0)
+	{
+		is_neg = true;
+		temp = input * -1;
+	}
+	else
+		temp = input;
+	
+	temp = ((temp<<8) * (env->level>>11)) >> 16;
+	int8_t out = temp;
+
+	if(is_neg == true)
+		 out *= -1;
+
+	env->sample = out;
+	return env->sample;
+}
+
---
+++ input.c
@@ -0,0 +1,56 @@
+// input.c
+#include "pico/stdlib.h"
+
+struct input_t
+{
+	uint8_t clk_pin;
+	uint8_t load_pin;
+	uint8_t ser_read;
+};
+
+void input_init(struct input_t* input, uint8_t clk_pin, uint8_t load_pin, uint8_t ser_read)
+{
+	// Init variables
+	input->clk_pin = clk_pin;
+	input->load_pin = load_pin;
+	input->ser_read = ser_read;
+	
+	// Init pins
+	gpio_init(input->clk_pin);
+	gpio_set_dir(input->clk_pin, GPIO_OUT);
+
+	gpio_init(input->load_pin);
+	gpio_set_dir(input->load_pin, GPIO_OUT);
+
+	gpio_init(input->ser_read);
+	gpio_set_dir(input->ser_read, GPIO_IN);
+
+}
+
+int input_read(struct input_t* input)
+{
+
+	// Load into register
+	gpio_put(input->load_pin, false);
+	busy_wait_at_least_cycles(15);
+
+	gpio_put(input->load_pin, true);
+
+	// Read register
+	// uint32_t data = 0;
+	int pos;
+	for (pos = 20; pos > 0; pos--)
+	{
+		// Read data
+		if(gpio_get(input->ser_read) == true)
+			break;
+
+		// Send clock signal
+		gpio_put(input->clk_pin, true);
+		busy_wait_at_least_cycles(15);
+		gpio_put(input->clk_pin, false);
+	}
+
+	return pos;
+}
+
